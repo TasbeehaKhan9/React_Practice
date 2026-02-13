115639f6da6467286167abd05f7b94a0dbfb4050
Tasbeeha Khan <tasbeehakhan05@gmail.com>
2026-02-14 02:24:05 +0500

    props practice

---
+++ my_react/src/App.jsx
@@ -0,0 +1,16 @@
+import Yes from "./props"
+
+function App() {
+ 
+const hobbies = ["reading , coding ,waticngmovies ,travelling"]
+
+  return (
+      
+    <>
+    <Yes name ="tasbeeha"  age ={20} cast = "sherwani pathan" hobbies ={hobbies}/>
+
+    </>
+  )
+}
+
+export default App
